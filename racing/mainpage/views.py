1e3785d05279a540fa2524d73f029408a7084e4f
Oleg1633 <oleg345567@gmail.com>
2024-08-27 21:47:31 +0300

    добавил фото

--- racing/mainpage/views.py
+++ racing/mainpage/views.py
@@ -4,7 +4,15 @@ def index(request):
     context={
         'participants':[
             {'name':'RedBull'},
-            {'name':'Mclaren'}
+            {'name':'Mclaren'},
+            {'name':'Ferrari'},
+            {'name':'Mercedes'},
+            {'name':'AstonMartin'},
+            {'name':'RacingBulls'},
+            {'name':'Haas'},
+            {'name':'Alpine'},
+            {'name':'Williams'},
+            {'name':'KickSauber'}
 
         ]
     }
